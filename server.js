57b176fe9871054a6cb2b142130fb372a41e9f9b
EllisLuisxD <152922476+EllisLuisxD@users.noreply.github.com>
2024-06-28 20:05:32 -0500

    server.js

--- server.js
+++ server.js
@@ -8,54 +8,48 @@ const cors = require('cors');
 
 const passport = require('passport');
 
-const usersRoutes=require('./routes/userRoutes');
+const usersRoutes = require('./routes/userRoutes');
 const multer = require('multer');
 
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended:true
-}  ));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./config/passport')(passport);
-
 app.disable('x-powered-by');
-
-
-app.set('port',port);
+app.set('port', port);
 
 const upload = multer({
-    storage:multer.memoryStorage()
-})
-
-usersRoutes(app,upload);
-
-server.listen(3000,'0.0.0.0'||'localhost',function(){
-    console.log("App de NodeJs "+process.pid+" Iniciada........")
+    storage: multer.memoryStorage()
 });
 
+usersRoutes(app, upload);
 
-//creando rutas
-app.get('/',(req,res)=>{
+// Configuración de server.listen()
+server.listen(port, 'localhost', function() {
+    console.log("App de NodeJs " + process.pid + " Iniciada en http://localhost:" + port);
+});
+
+// Creando rutas
+app.get('/', (req, res) => {
     res.send('Ruta raíz del backend');
 });
 
-app.get('/test',(req,res)=>{
+app.get('/test', (req, res) => {
     res.send("Esta es la ruta test");
-})
-
-//manejo de errores
-app.use((err,req,res,next)=>{
-    console.log(err);
-    res.status(err.status||500).send(err.stack);
 });
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).send('Error en el servidor: ' + err.message);
+});
+
 
 
 
