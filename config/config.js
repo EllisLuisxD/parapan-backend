8f148f3fc5c232f5cf2ccf7b962fcb635a20fb4f
EllisLuisxD <152922476+EllisLuisxD@users.noreply.github.com>
2024-06-28 20:05:11 -0500

    config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'bspgubhn8wjnxbva6j8d-mysql.services.clever-cloud.com',
     port : 3306,
     user: 'u3yhsqsow1d97jfu',
-    password: 'u3yhsqsow1d97jfu',
+    password: 'eG2uTrFY3uAig4qIzIEo',
     database: 'bspgubhn8wjnxbva6j8d'
 })
 
